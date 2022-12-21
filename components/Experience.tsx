68529ea44fe3a002ed131e4acecdf780d1b11690
adiozdaniel <42722945+adiozdaniel@users.noreply.github.com>
2022-12-21 11:30:44 +0300

    prisma

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -10,6 +10,7 @@ import NodeCard from './NodeCard';
 import TailwindCard from './TailwindCard';
 import TrpcCard from './TrpcCard';
 import NextJs from './Nextjs';
+import Prisma from './Prisma';
 
 type Props = {}
 
@@ -37,6 +38,7 @@ export default function Experience({}: Props) {
                 <TailwindCard/>
                 <TrpcCard/>
                 <NextJs/>
+                <Prisma/>
             </Carousel>
           
         </motion.div>
---
+++ components/Prisma.tsx
@@ -0,0 +1,40 @@
+import { motion } from "framer-motion"
+import { TbBrandJavascript,
+    TbBrandHtml5,
+    TbBrandCss3 } from 'react-icons/tb';
+
+
+type Props = {}
+
+export default function Prisma({}: Props) {
+    return (
+        <div className="md:inline-flex flex flex-col md:flex-row justify-center mt-16 rounded-lg items-center
+        space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center 
+         p-10 hover:opacity-100 opacity-40 cursor-pointer
+        transition-opacity duration-200 overflow-hidden">
+            <motion.img 
+                initial={{
+                    y: -100,
+                    opacity: 0,
+                }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true}}
+                className="max-w-[480px]  pr-28 text-blue-600"
+                src="https://res.cloudinary.com/dashoh/image/upload/v1671611398/avatars/dashoInc/lsx37oa94gvlbuuqsg8i.png"
+                alt='Node'           
+            />        
+
+                         
+                
+                {/* <div className="flex space-x-2 my-2">
+                    <TbBrandJavascript className="h-10 w-10 rounded-full text-2xl"/>
+                    <TbBrandHtml5 className="h-10 w-10 rounded-full text-2xl"/>
+                    <TbBrandCss3 className="h-10 w-10 rounded-full text-2xl"/>
+                </div> */}
+                   
+          
+          
+        </div>
+    )
+}
